fb23d49dfa97d60eac99d1524a59ca7d0d026ca7
Augusto Sudbrack <eng.augusto.sudbrack@hotmail.com>
2021-10-24 11:33:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'Zer0bito',
+  youtube: 'channel/UCf3S8RUxwDZZz9-fKoVqfgw',
+  facebook: 'augusto.sudbrack',
+  instagram: 'asudbrack',
+  twitter: 'asudbrack'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
